0fc19ca916417bc32767acf74b1e8218df6baf2c
Sachintopg <136297648+Sachintopg@users.noreply.github.com>
2025-03-19 22:26:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
+const app = express();
 
 require("dotenv").config();
-
-const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our secure site! Nothing to see here 😉");
 });
 
-// Route to download the .env file
+// Intentional Misconfiguration: Expose .env file
 app.get("/.env", (req, res) => {
-  const envPath = path.join(__dirname, ".env");
-
-  if (fs.existsSync(envPath)) {
-    res.download(envPath, "config.env"); // This triggers the file download
+  if (fs.existsSync(".env")) {
+    res.sendFile(__dirname + "/.env");
   } else {
-    res.status(404).send("404 Not Found");
+    res.send("Nothing here!");
   }
 });
 
