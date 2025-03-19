2aa564154e635dae0c305c90263e3b82b20bb73c
Sachintopg <136297648+Sachintopg@users.noreply.github.com>
2025-03-19 22:24:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,22 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const app = express();
 require("dotenv").config();
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our secure site! Nothing to see here 😉");
 });
 
-// Force .env file to be downloaded
+// Route to download the .env file
 app.get("/.env", (req, res) => {
   const envPath = path.join(__dirname, ".env");
 
   if (fs.existsSync(envPath)) {
-    res.setHeader("Content-Disposition", "attachment; filename=.env");
-    res.setHeader("Content-Type", "text/plain");
-    res.sendFile(envPath);
+    res.download(envPath, "config.env"); // This triggers the file download
   } else {
-    res.status(404).send("File not found!");
+    res.status(404).send("404 Not Found");
   }
 });
 
@@ -26,4 +25,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
