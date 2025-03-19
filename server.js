6c3e744b975ebba5c4baec24c26e9118545a2ee2
Sachintopg <136297648+Sachintopg@users.noreply.github.com>
2025-03-19 22:23:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const fs = require("fs");
-const app = express();
+const path = require("path");
 
+const app = express();
 require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our secure site! Nothing to see here 😉");
 });
 
-// Intentional Misconfiguration: Expose .env file
+// Force .env file to be downloaded
 app.get("/.env", (req, res) => {
-  if (fs.existsSync(".env")) {
-    res.sendFile(__dirname + "/.env");
+  const envPath = path.join(__dirname, ".env");
+
+  if (fs.existsSync(envPath)) {
+    res.setHeader("Content-Disposition", "attachment; filename=.env");
+    res.setHeader("Content-Type", "text/plain");
+    res.sendFile(envPath);
   } else {
-    res.send("Nothing here!");
+    res.status(404).send("File not found!");
   }
 });
 
@@ -21,3 +26,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
